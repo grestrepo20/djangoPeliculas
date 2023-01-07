85a6aef093edec4a03b01fe4d49f2120d5682117
grestrepo20 <geraldin2017gmail.com>
2023-01-07 17:43:52 -0500

    Primer envio

---
+++ myproject/peliculas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+  path('', views.inicio, name='inicio'),
+  path('ayuda', views.ayuda, name='ayuda'),
+  path('peliculas', views.peliculas, name='peliculas'),
+  path('peliculas/agregar', views.agregar, name='agregar'),
+  path('peliculas/editar/<int:id>', views.editar, name='editar'),
+  path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myproject/peliculas/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import Pelicula
+from .forms import PeliculaForm
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+
+def ayuda(request):
+    return render(request, 'paginas/ayuda.html')
+
+def peliculas(request):
+    peliculas = Pelicula.objects.all()
+    return render(request,'peliculas/index.html', {'peliculas':peliculas})
+
+def agregar(request):
+    formulario = PeliculaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('peliculas')
+    return render(request, 'peliculas/agregar.html', {'formulario': formulario})
+
+def editar(request, id):
+    pelicula = Pelicula.objects.get(id=id)
+    formulario = PeliculaForm(request.POST or None, request.FILES or None, instance=pelicula)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('peliculas')
+    return render(request, 'peliculas/editar.html',{'formulario':formulario})
+
+def eliminar(request,id):
+    pelicula = Pelicula.objects.get(id=id)
+    pelicula.delete()
+    return redirect('peliculas') 
+    
+    
+    
